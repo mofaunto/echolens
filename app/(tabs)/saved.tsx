ecd2868a78107b84ccc62847cc526dbe0634f705
Khozhimatov Muzaffar <xjmuzaffar@gmail.com>
2025-04-02 20:56:53 +0900

    Simple saves page is done

--- app/(tabs)/saved.tsx
+++ app/(tabs)/saved.tsx
@@ -1,10 +1,67 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
 import React from "react";
+import { useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import Loader from "@/components/Loader";
+import { colors } from "@/constants/theme";
+import { styles } from "@/styles/feed.styles";
+import { Image } from "expo-image";
 
 export default function Saved() {
+    const savedPosts = useQuery(api.saves.getSaves);
+
+    if (savedPosts === undefined) return <Loader />;
+    if (savedPosts.length === 0) return <NoSavesFound />;
+
     return (
-        <View>
-            <Text>saved</Text>
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Text style={styles.headerTitle}>Saved posts</Text>
+            </View>
+
+            <ScrollView
+                contentContainerStyle={{
+                    padding: 8,
+                    flexDirection: "row",
+                    flexWrap: "wrap",
+                }}
+            >
+                {savedPosts.map((post) => {
+                    if (!post) return null;
+
+                    return (
+                        <View
+                            key={post._id}
+                            style={{ width: "33.33%", padding: 1 }}
+                        >
+                            <Image
+                                source={post.imageUrl}
+                                style={{ width: "100%", aspectRatio: 1 }}
+                                contentFit="cover"
+                                transition={200}
+                                cachePolicy="memory-disk"
+                            />
+                        </View>
+                    );
+                })}
+            </ScrollView>
         </View>
     );
 }
+
+const NoSavesFound = () => {
+    return (
+        <View
+            style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+                backgroundColor: colors.background,
+            }}
+        >
+            <Text style={{ color: colors.primary, fontSize: 20 }}>
+                No saved posts
+            </Text>
+        </View>
+    );
+};
